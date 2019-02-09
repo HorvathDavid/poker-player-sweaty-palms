b66f9f5934445263feba47525ad02cba5952259c
dawe <horvath.dawe@gmail.com>
2019-02-09 13:34:03 +0100

    whichGroup suited test

--- test.js
+++ test.js
@@ -53,12 +53,12 @@ var a = {
             "hole_cards": [                         // The cards of the player. This is only visible for your own player
                                                     //     except after showdown, when cards revealed are also included.
                 {
-                    "rank": "9",                    // Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+                    "rank": "A",                    // Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
                     "suit": "hearts"                // Suit of the card. Possible values are: clubs,spades,hearts,diamonds
                 },
                 {
-                    "rank": "9",
-                    "suit": "spades"
+                    "rank": "Q",
+                    "suit": "clubs"
                 }
             ]
         },
