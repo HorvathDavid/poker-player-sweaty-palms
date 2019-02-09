4166f111e7d8a034c430f55de0267e0935ef3974
adamtoth <tot.adam@gmail.com>
2019-02-09 10:28:42 +0100

    Raise high cards. v0.2

--- Player.js
+++ Player.js
@@ -1,9 +1,21 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return '0.2';
   }
 
   static betRequest(gameState, bet) {
+    // Init
+    const {current_buy_in, minimum_raise, players, in_action} = gameState
+
+    let tempCards = ['10', 'J', 'Q', 'K', 'A'];
+    let ourID = gameState.in_action;
+    let ourCards = players.find(obj => obj.id == ourID).hole_cards;
+    
+    if (tempCards.includes(ourCards[0].rank) && tempCards.includes(ourCards[1].rank)) {
+      let betzor = current_buy_in - players[in_action][bet] + minimum_raise;
+      bet(Math.floor(betzor));
+    }
+
     bet(0);
   }
 
